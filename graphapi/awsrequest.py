6721b4e5c82e0023b4aff70cb46e34cb71cf37f1
Jay Yoo <jayyoo@Jays-MacBook-Pro.local>
2024-02-08 00:43:10 -0800

    accessed aws dynamodb using boto3

--- graphapi/awsrequest.py
+++ graphapi/awsrequest.py
@@ -1,43 +1,31 @@
-import requests
-from requests_aws4auth import AWS4Auth
+import decimal
+import json
+import logging
+import os
+import pprint
+import time
+import boto3
+from botocore.exceptions import ClientError
 
-# Amplify.configure({
-#   API: {
-#     GraphQL: {
-#       endpoint: 'https://ljrju4chcbeftbp6fjp7j4ke4q.appsync-api.us-west-1.amazonaws.com/graphql',
-#       region: 'us-west-1',
-#       defaultAuthMode: 'apiKey',
-#       apiKey: 'da2-n2bao*********************'
-#     }
-#   }
-# });
+logger = logging.getLogger(__name__)
+dynamodb = boto3.resource("dynamodb")
 
-# Use AWS4Auth to sign a requests session
-session = requests.Session()
-session.auth = AWS4Auth(
-    # An AWS 'ACCESS KEY' associated with an IAM user.
-    'AKxxxxxxxxxxxxxxx2A',
-    # The 'secret' that goes with the above access key.                    
-    'kwWxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxgEm',    
-    # The region you want to access.
-    'us-west-1',
-    # The service you want to access.
-    'appsync'
+MAX_GET_SIZE = 100  # Amazon DynamoDB rejects a get batch larger than 100 items.
+
+dynamodb = boto3.client("dynamodb")
+TABLE_NAME = "Todo-zirl2v2iw5h7tnbs3wznskuvvm-bulkupcoac"
+
+# Creating the DynamoDB Client
+dynamodb_client = boto3.client('dynamodb', region_name="us-west-1")
+
+# Creating the DynamoDB Table Resource
+dynamodb = boto3.resource('dynamodb', region_name="us-west-1")
+table = dynamodb.Table(TABLE_NAME)
+
+response = dynamodb_client.get_item(
+    TableName=TABLE_NAME,
+    Key={
+        'id': {'S': 'd138d559-b08b-4495-878b-32059d1cab99'}
+    }
 )
-# As found in AWS Appsync under Settings for your endpoint.
-APPSYNC_API_ENDPOINT_URL = 'https://ljrju4chcbeftbp6fjp7j4ke4q.appsync-api.us-west-1.amazonaws.com/graphql'
-# Use JSON format string for the query. It does not need reformatting.
-query = """
-    query foo {
-        GetUserSettings (
-           identity_id: "ap-southeast-2:8xxxxxxb-7xx4-4xx4-8xx0-exxxxxxx2"
-        ){ 
-           user_name, email, whatever 
-}}"""
-# Now we can simply post the request...
-response = session.request(
-    url=APPSYNC_API_ENDPOINT_URL,
-    method='POST',
-    json={'query': query}
-)
-print(response.text)
+print(response['Item'])
