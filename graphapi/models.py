5dbc91bef9f3deb051ff3cc60e032bb51106fcbd
Jay Yoo <jayyoo@Jays-MacBook-Pro.local>
2024-02-16 22:23:38 -0800

    models modified

--- graphapi/models.py
+++ graphapi/models.py
@@ -2,24 +2,26 @@ from django.db import models
 
 class Sleep(models.Model):
     id = models.UUIDField(primary_key=True)
-    end_at = models.DateTimeField(null=True)
-    start_at = models.DateTimeField(null=True)
-    user_id = models.UUIDField(db_index=True)
+    endAt = models.DateTimeField(null=True)
+    startAt = models.DateTimeField(null=True)
+    userID = models.UUIDField(db_index=True)
 
 class User(models.Model):
     id = models.UUIDField(primary_key=True)
-    bmi = models.FloatField(null=True)
-    
+    BMI = models.FloatField(null=True)
+
 class Exercise(models.Model):
     id = models.UUIDField(primary_key=True)
     name = models.CharField(max_length=255)
-    completed_at = models.DateTimeField(null=True)
+    weight_lb = models.IntegerField(null=True)
+    reps = models.IntegerField(null=True)
+    completedAt = models.DateTimeField(null=True)
     target = models.CharField(max_length=255)
-    user_id = models.UUIDField(db_index=True)
+    userID = models.UUIDField(db_index=True)
 
 class Protein(models.Model):
     id = models.UUIDField(primary_key=True)
     name = models.CharField(max_length=255)
     grams = models.FloatField()
-    completed_at = models.DateTimeField(null=True)
-    user_id = models.UUIDField(db_index=True)
+    completedAt = models.DateTimeField(null=True)
+    userID = models.UUIDField(db_index=True)
